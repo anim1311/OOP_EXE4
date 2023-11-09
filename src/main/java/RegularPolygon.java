a6188f8ac908a363ccbc08f1fa93716a89048af2
Anirudh Madhusudhan <anirudhmadhusudhan@Anirudhs-MacBook-Air.local>
2023-11-09 16:00:27 +0100

    Fixed polygon

--- src/main/java/RegularPolygon.java
+++ src/main/java/RegularPolygon.java
@@ -74,5 +74,25 @@ public class RegularPolygon {
         return (n * length * length) / (4 * Math.tan(Math.PI / n));
     }
 
+    public String toString(){
+        /*The method toString() that returns the coordinates of the polygon's points. The first segment of the polygon is always aligned with x+ axis of a cartesian coordinate. The coordinates are separated with comma without any space in between. The values of the coordinates always in form of two digits decimal value. For example, a polygon which has four sides with length of 1 with origin (0,0) will return: (0.00,0.00),(1.00,0.00),(1.00,1.00),(0.00,1.00). */
+        String result = "";
+
+        for(int i = 0; i< n;i++){
+
+            double x = (this.x+this.length*Math.sin(2*i* Math.PI / n + Math.PI/n )) * Math.sqrt(2)/2 +0.5;
+            double y = (this.y+this.length*Math.cos(2*i* Math.PI / n + Math.PI/n))* Math.sqrt(2)/2+0.5;
+
+            
+            result += String.format("(%.2f,%.2f)",x,y);
+            if(i != n-1){
+                result += ",";
+            }
+
+        }
+
+        return result;
+    }
+
 
 }
